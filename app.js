421e17996576ddbda305a2f3dbc5f00bb8332b64
Caio Mafra <caiomafra10@gmail.com>
2024-06-04 23:42:36 -0300

    .

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import connectDB from './config/db.js';
-import dotenv from 'dotenv';
-import cors from 'cors';
+import express from "express";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
@@ -9,9 +9,12 @@ const app = express();
 
 // Conectar ao banco de dados
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
@@ -19,16 +22,31 @@ app.use(function(req, res, next) {
 app.use(express.json({ extended: false }));
 
 // Rotas
-import eventosRoutes from './routes/eventos.js';
-import inscricoesRoutes from './routes/inscricoes.js';
-import authRoutes from './routes/auth.js';
+import eventosRoutes from "./routes/eventos.js";
+import inscricoesRoutes from "./routes/inscricoes.js";
+import authRoutes from "./routes/auth.js";
 
-app.get('/', () => {
-  return 'Ola Mundi'
-})
+app.get("/", () => {
+  return "Ola Mundo";
+});
 
-app.use('/api/eventos', eventosRoutes);
-app.use('/api/inscricoes', inscricoesRoutes);
-app.use('/api/auth', authRoutes);
+app.use("/api/eventos", eventosRoutes);
+app.use("/api/inscricoes", inscricoesRoutes);
+app.use("/api/auth", authRoutes);
+
+if (process.env.LOCAL === "production") {
+  const privateKey = fs.readFileSync("privatekey.pem", "utf8");
+  const certificate = fs.readFileSync("certificate.pem", "utf8");
+
+  const credentials = { key: privateKey, cert: certificate };
+
+  // Crie o servidor HTTPS
+  const httpsServer = https.createServer(credentials, app);
+
+  // Inicie o servidor HTTPS
+  httpsServer.listen(443, () => {
+    console.log("HTTPS Server running on port 443");
+  });
+}
 
 export default app;
